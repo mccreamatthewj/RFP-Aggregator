c2bdcaac62c8e63db88ae4b6683f174631a71566
Matthew McCrea <mccrea.matthewj@gmail.com>
2026-02-11 12:35:15 -0600

    Fix import path issue by adding the parent directory to sys.path in scripts/test_crawler.py

--- scripts/test_crawler.py
+++ scripts/test_crawler.py
@@ -1,5 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Rest of your code here...
+# Add the parent directory to sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+# Existing import statements below this line
