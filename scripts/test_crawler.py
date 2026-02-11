9c562f53017a76e843deb5e0db3b3d1394f2549a
Matthew McCrea <mccrea.matthewj@gmail.com>
2026-02-11 12:34:20 -0600

    Fix import path by adding the project root to sys.path

--- scripts/test_crawler.py
+++ scripts/test_crawler.py
@@ -1,10 +1,5 @@
 import sys
 import os
-
-# Add the parent directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Now you can import the crawler correctly
-from crawlers.indiana_crawler import IndianaCrawler
-
-# The rest of your code...
+# Rest of your code here...
